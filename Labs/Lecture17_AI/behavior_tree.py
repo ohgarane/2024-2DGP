4346db7bffc3c21748b63ea18fcf434365781bc1
ohgarane <ohgarane8@naver.com>
2024-11-26 12:41:54 +0900

    Update behavior_tree.py

--- Labs/Lecture17_AI/behavior_tree.py
+++ Labs/Lecture17_AI/behavior_tree.py
@@ -12,6 +12,15 @@ def print_indent():
     for i in range(level):
         print("    ", end='')
 
+def build_behavior_tree(self):
+    a1 = Action('Set target location', self.set_target_location, 1000, 1000)
+    a2 = Action('Move to', self.move_to)
+    root = move_to_target_location = Sequence('Move to target location', a1, a2)
+    self.bt = BehaviorTree(root)
+
+def update(self):
+ self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % FRAMES_PER_ACTION
+ self.bt.run()
 
 class BehaviorTree:
     FAIL, RUNNING, SUCCESS = -1, 0, 1
@@ -88,15 +97,6 @@ class Selector(Node):
         self.value = BehaviorTree.FAIL
         return self.value
 
-
-
-
-
-
-
-
-
-
 class Sequence(Node):
     def __init__(self, name, *nodes):
         self.children = list(nodes)
@@ -156,7 +156,29 @@ class Action(Node):
     def run(self):
         self.value = self.func(*self.args)
         return self.value
+    def move_to(self, r = 0.5):
+        self.state = 'Walk'
+        self.move_slightly_to(self.tx, self.ty)
+        if self.distance_less_than(self.tx, self.ty, self.x, self.y, r):
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.RUNNING
 
+    def set_target_location(self, x=None, y=None):
+        if not x or not y:
+            raise ValueError('Location should be given')
+        self.tx, self.ty = x, y
+        return BehaviorTree.SUCCESS
+
+    def distance_less_than(self, x1, y1, x2, y2, r):
+        distance2 = (x1 - x2) ** 2 + (y1 - y2) ** 2
+        return distance2 < (PIXEL_PER_METER * r) ** 2
+
+    def move_slightly_to(self, tx, ty):
+        self.dir = math.atan2(ty - self.y, tx - self.x)
+        distance = RUN_SPEED_PPS * game_framework.frame_time
+        self.x += distance * math.cos(self.dir)
+        self.y += distance * math.sin(self.dir)
 
     # @Node.show_result
     # def monitor_run(self):
